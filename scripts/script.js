c501aa63b351442d6be4252f4c434f769c3e26f0
Kolpaxidis V. (laptop) <kolpaxidis86@gmail.com>
2023-07-04 11:20:30 +0300

    Add Erase and Erase all data. Change some styles and add new styles.

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ import { displayErrors } from "./displayErrors.js";
 const cards = document.querySelectorAll('.card');
 const nextButtons = document.querySelectorAll('.nextBtn');
 const prevButtons = document.querySelectorAll('.prevBtn');
+const eraseAllBtn = document.querySelector('.eraseAllData');
+const eraseCurrentBtn = document.querySelector('.eraseData');
 let currentCard = 0;
 
 function showNextCard() {
@@ -14,7 +16,8 @@ function showNextCard() {
 	} else {
 		cards[currentCard].style.display = 'block';
 	}
-}
+};
+
 function showPrevCard() {
 	cards[currentCard].style.display = 'none';
 
@@ -23,10 +26,39 @@ function showPrevCard() {
 	if (currentCard < 0) {
 		alert('Form start')
 	} else {
-
 		cards[currentCard].style.display = 'block';
 	}
 }
+
+eraseCurrentBtn.addEventListener('click', () => {
+	const inputs = Array.from(document.querySelectorAll('input')); // return array not nodeList
+	const allEmpty = inputs.every(input => input.value == '');
+	if (!allEmpty) {
+		const conf = confirm("Are you sure you want to delete all the data on all the cards?");
+		if (conf) {
+			inputs.forEach(input => {
+				input.value = '';
+			});
+		} else {
+			return;
+		}
+	} else {
+		alert("There's nothing to erase here")
+	};
+});
+
+eraseAllBtn.addEventListener('click', () => {
+	const conf = confirm("Are you sure you want to delete data from current card?");
+	if (conf) {
+		const currentCardElements = cards[currentCard];
+		const inputs = currentCardElements.querySelectorAll('input');
+		inputs.forEach(input => {
+			input.value = '';
+		})
+	} else {
+		return;
+	};
+});
 
 nextButtons.forEach((button, index) => {
 	button.addEventListener('click', (event) => {
--- scripts/validateInput.js
+++ scripts/validateInput.js
@@ -8,4 +8,4 @@ export function validateInput(currentCard) {
 		}
 	});
 	return invalidInputs;
-}
+};
